714cd94112e0d5f46f6bd6913121b48c08cde9b2
Rewind <sploing1@gmail.com>
2017-08-06 22:16:24 -0700

    basic window

---
+++ src/com/rs/game/panels/GamePanel.java
@@ -0,0 +1,20 @@
+package com.rs.game.panels;
+
+import com.rs.engine.display.Camera;
+import com.rs.engine.display.Window;
+
+public class GamePanel {
+
+	public GamePanel(Window window, Camera camera) {
+		
+	}
+
+	public void update() {
+		
+	}
+
+	public void render() {
+		
+	}
+
+}
